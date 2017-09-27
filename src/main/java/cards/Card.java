9c24cb33395d99f885dafc4d74fa83283fd0ccba
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2017-09-27 15:37:58 +0300

    Cards implemented.

--- src/main/java/cards/Card.java
+++ src/main/java/cards/Card.java
@@ -1,18 +1,83 @@
 package cards;
 
 public class Card {
-    enum Suit {}
-    enum Value {}
+
+    public enum Value {
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE
+    }
+
+    public enum Suit {
+        DIAMONDS,
+        SPADES,
+        CLUBS,
+        HEARTS
+    }
+
+    private final Suit suit;
+    private final Value value;
+
+    public Card(Value value, Suit suit) {
+        this.suit = suit;
+        this.value = value;
+    }
 
     public Suit getSuit() {
-        return null;
+        return suit;
     }
 
-    public int getValue(int points) {
-        return 0;
+    public int getPoints(int points) {
+        switch (value) {
+            case TWO: return 2;
+            case THREE: return 3;
+            case FOUR: return 4;
+            case FIVE: return 5;
+            case SIX: return 6;
+            case SEVEN: return 7;
+            case EIGHT: return 8;
+            case NINE: return 9;
+            case TEN: return 10;
+            case JACK: return 2;
+            case QUEEN: return 3;
+            case KING: return 4;
+            case ACE:
+                if (points + 11 > 21) return 1;
+                return 11;
+            default:
+                throw new UnsupportedOperationException("Broken card.");
+        }
     }
 
-    public String getName() {
-        return null;
+    public Value getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Card)) return false;
+
+        Card card = (Card) o;
+
+        if (suit != card.suit) return false;
+        return value == card.value;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
     }
 }
--- src/test/java/CardsTest.java
+++ src/test/java/CardsTest.java
@@ -7,34 +7,72 @@ import static org.junit.Assert.assertTrue;
 public class CardsTest {
 
     @Test
-    public void getValue() {
-        Card card = new Card();
-        assertTrue(card.getValue(0) != 0);
+    public void getPoints() {
+        Card card = new Card(Card.Value.JACK, Card.Suit.DIAMONDS);
+        assertTrue(card.getPoints(12) == 2);
+        card = new Card(Card.Value.TWO, Card.Suit.HEARTS);
+        assertTrue(card.getPoints(2) == 2);
+        card = new Card(Card.Value.TEN, Card.Suit.CLUBS);
+        assertTrue(card.getPoints(3) == 10);
+        card = new Card(Card.Value.TEN, Card.Suit.HEARTS);
+        assertTrue(card.getPoints(5) == 10);
+        card = new Card(Card.Value.NINE, Card.Suit.SPADES);
+        assertTrue(card.getPoints(17) == 9);
+        card = new Card(Card.Value.QUEEN, Card.Suit.DIAMONDS);
+        assertTrue(card.getPoints(123) == 3);
+        card = new Card(Card.Value.KING, Card.Suit.HEARTS);
+        assertTrue(card.getPoints(92) == 4);
     }
 
     @Test
     public void getSuit() {
-        Card card = new Card();
-        assertTrue(card.getSuit() != null);
+        Card card = new Card(Card.Value.JACK, Card.Suit.DIAMONDS);
+        assertTrue(card.getSuit() == Card.Suit.DIAMONDS);
+        card = new Card(Card.Value.ACE, Card.Suit.SPADES);
+        assertTrue(card.getSuit() == Card.Suit.SPADES);
     }
 
     @Test
-    public void getName() {
-        Card card = new Card();
-        assertTrue(card.getName() != null);
+    public void getValue() {
+        Card card = new Card(Card.Value.JACK, Card.Suit.DIAMONDS);
+        assertTrue(card.getValue() == Card.Value.JACK);
+        card = new Card(Card.Value.JACK, Card.Suit.CLUBS);
+        assertTrue(card.getValue() == Card.Value.JACK);
+        card = new Card(Card.Value.JACK, Card.Suit.HEARTS);
+        assertTrue(card.getValue() == Card.Value.JACK);
+        card = new Card(Card.Value.QUEEN, Card.Suit.DIAMONDS);
+        assertTrue(card.getValue() == Card.Value.QUEEN);
     }
 
     @Test
-    public void getAceValue() {
-        Card card = new Card();
-        assertTrue(card.getValue(0) != 0);
+    public void getAcePoints() {
+        Card card = new Card(Card.Value.ACE, Card.Suit.SPADES);
+        assertTrue(card.getPoints(0) == 11);
+        assertTrue(card.getPoints(5) == 11);
+        assertTrue(card.getPoints(10) == 11);
+        assertTrue(card.getPoints(11) == 1);
+        assertTrue(card.getPoints(20) == 1);
     }
 
     @Test
     public void testEquals() {
-        Card card1 = new Card();
-        Card card2 = new Card();
+        Card card1 = new Card(Card.Value.ACE, Card.Suit.SPADES);
+        Card card2 = new Card(Card.Value.ACE, Card.Suit.HEARTS);
         assertFalse(card1.equals(card2));
         assertFalse(card2.equals(card1));
+
+        card1 = new Card(Card.Value.TWO, Card.Suit.SPADES);
+        card2 = new Card(Card.Value.ACE, Card.Suit.SPADES);
+        assertFalse(card1.equals(card2));
+        assertFalse(card2.equals(card1));
+
+        card1 = new Card(Card.Value.TWO, Card.Suit.CLUBS);
+        card2 = new Card(Card.Value.TWO, Card.Suit.CLUBS);
+        assertTrue(card1.equals(card2));
+        assertTrue(card2.equals(card1));
+
+        card1 = new Card(Card.Value.TWO, Card.Suit.SPADES);
+        card2 = null;
+        assertFalse(card1.equals(card2));
     }
 }
