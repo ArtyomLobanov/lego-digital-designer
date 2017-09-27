6269d5f205b7516b34b31122a04335076d204936
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2017-09-27 15:44:44 +0300

    Deck implemented.

--- src/main/java/cards/Deck.java
+++ src/main/java/cards/Deck.java
@@ -1,7 +1,28 @@
 package cards;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Deck {
-    public Card getCard() {
-        return null;
+
+    private final List<Card> cards = new ArrayList<>();
+    private int position = 0;
+
+    public Deck() {
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Value value : Card.Value.values()) {
+                cards.add(new Card(value, suit));
+            }
+        }
+        Collections.shuffle(cards, new Random(228));
+    }
+
+    public Card getCard() throws DeckIsEmptyException {
+        if (position >= cards.size()) {
+            throw new DeckIsEmptyException();
+        }
+        return cards.get(position++);
     }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,27 +1,39 @@
 import cards.Card;
 import cards.Deck;
+import cards.DeckIsEmptyException;
 import org.junit.Test;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertNotNull;
 
 public class DeckTest {
 
     @Test
-    public void getCard() {
-        assertTrue(new Deck().getCard() != null);
+    public void getCard() throws DeckIsEmptyException {
+        Card card = new Deck().getCard();
+        assertNotNull(card);
+        assertNotNull(card.getSuit());
+        assertNotNull(card.getValue());
     }
 
     @Test
-    public void differentCards() {
+    public void differentCards() throws DeckIsEmptyException {
         Set<Card> cards = new HashSet<>();
         Deck deck = new Deck();
         for (int i = 0; i < 52; i++) {
             cards.add(deck.getCard());
         }
         assertEquals(52, cards.size());
+    }
+
+    @Test(expected = DeckIsEmptyException.class)
+    public void isEmpty() throws DeckIsEmptyException {
+        Deck deck = new Deck();
+        for (int i = 0; i < 53; i++) {
+            deck.getCard();
+        }
     }
 }
